c584d5e22b2f791d3a13e431d707c97310bdd7a8
TWickeryRules <TWickery@gmail.com>
2017-09-04 17:05:30 +0100

    New Commit

--- Bots/OhGodMusicBot/server.js
+++ Bots/OhGodMusicBot/server.js
@@ -1,8 +1,8 @@
-const { Client } = require('discord.js');
+const Discord = require('discord.js');
 const yt = require('ytdl-core');
 const tokens = require('./tokens.json');
 
-const client = new Client();
+const client = new Discord.Client();
 
 let queue = {};
 
