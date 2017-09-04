c59d9552c487de330211caee0c1fd1351b1f6026
TWickeryRules <TWickery@gmail.com>
2017-09-04 13:37:13 +0100

    New Commit

--- Bots/MySelfBot/index.js
+++ Bots/MySelfBot/index.js
@@ -11,7 +11,7 @@ client.on('ready', function() {
 
 client.on('message', function(message) {
     if (message.author !== client.user) return;
-    console.log(message)
+    console.log(message.content)
     const args = message.content.split(/\s + /g)
     const command = args.shift().slice(prefix.length).toLowerCase();
     switch (command) {
