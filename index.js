8f639040f7dfbc929ba3b22254da902f0f70c16e
TWickeryRules <TWickery@gmail.com>
2017-09-04 15:48:47 +0100

    New Commit

--- index.js
+++ index.js
@@ -271,12 +271,10 @@ client.on('message', function(message) {
             var server = servers[message.guild.id]
             server.queue.push(args[0])
             console.log("And Here")
-            if (!client.voiceConnections.has(message.member.voiceChannel)) {
-                message.member.voiceChannel.join().then(function(connection) {
-                    play(connection, message)
-                    console.log("Same Here")
-                })
-            }
+            message.member.voiceChannel.join().then(function(connection) {
+                play(connection, message)
+                console.log("Same Here")
+            })
         break;
         case "skip":
             var server = servers[message.guild.id]
